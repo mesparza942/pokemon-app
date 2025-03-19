a46a74f99e3113979f4fe66e932754d1f7ee077e
mesparza942 <maresparzam@gmail.com>
2025-03-19 12:36:15 -0500

    Fix serialization console error caused by redux-persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist/es/constants";
 import userReducer from "./userSlice";
 
 const persistConfig = {
@@ -14,6 +22,12 @@ export const store = configureStore({
   reducer: {
     user: persistedUserReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
